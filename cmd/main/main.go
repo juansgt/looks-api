4a77234ad91bec53e2acdb4d8032fdc2e4cf3baf
juansgt <32044146+juansgt@users.noreply.github.com>
2023-01-30 00:46:46 +0100

    Primera integración con Mongodb en atlas haciendo uso de model-test v3.0.0-beta.5

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"context"
 	"look-api/pkg/controllers"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/juansgt/model-test/v3/dataAccess/lookRepository"
 	"github.com/juansgt/model-test/v3/services/findLooksService"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var looksController *controllers.LooksController
+var database *mongo.Database
+var mongoClient *mongo.Client
 
 func initializeDependencies() {
-	looksController = controllers.NewLooksController(findLooksService.NewFindLooksQueryService(new(lookRepository.LookRepositoryMock)))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://atlasAdmin:Cripto0Virtual@cluster0.yolpv.mongodb.net/?retryWrites=true&w=majority"))
+	if err != nil {
+		panic(err)
+	}
+	mongoClient = client
+	database = mongoClient.Database("wap")
+	looksController = controllers.NewLooksController(findLooksService.NewFindLooksQueryService(lookRepository.NewLookRepositoryMongodb(database)))
 }
 
 func main() {
